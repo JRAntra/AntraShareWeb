40b2e59d92a777b3c4fb0e9f4bb728b0bca90023
Yusheng Zhang <yusheng.zhang@antra.com>
2022-09-07 14:46:39 -0400

    test commit for all unstaged files

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -4,14 +4,23 @@ import { CommonModule } from '@angular/common';
 import { LoginRoutingModule } from './login-routing.module';
 import { Routes } from '@angular/router';
 import { LoginPageComponent } from './components/login-page/login-page.component';
+import { LoginUserPanelComponent } from './components/login-page/login-user-panel/login-user-panel.component';
+import { LoginLogoPanelComponent } from './components/login-page/login-logo-panel/login-logo-panel.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LoginPageComponent,
+    LoginUserPanelComponent,
+    LoginLogoPanelComponent
+  ],
   imports: [
     CommonModule,
     LoginRoutingModule
+  ],
+  exports:[
+
   ]
 })
 export class LoginModule { }
