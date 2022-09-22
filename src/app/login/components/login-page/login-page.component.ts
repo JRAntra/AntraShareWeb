593b41b8b5ca87ed0f538adeebb6a57b691e0269
Yusheng Zhang <yusheng.zhang@antra.com>
2022-09-22 17:38:48 -0400

    add validators

--- src/app/login/components/login-page/login-page.component.ts
+++ src/app/login/components/login-page/login-page.component.ts
@@ -1,23 +1,40 @@
-import { AbstractControl, AsyncValidatorFn, FormControl, FormGroup,FormArray, ValidationErrors, Validators } from '@angular/forms';
+import { AbstractControl, AsyncValidatorFn, FormControl, FormBuilder, FormGroup, FormArray, ValidationErrors, Validators } from '@angular/forms';
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Router } from '@angular/router';
-import { of } from 'rxjs';
-import { FormBuilder } from '@angular/forms';
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.sass']
 })
+
+
 export class LoginPageComponent implements OnInit {
 
-  
+
   constructor(private router: Router,
     private fb: FormBuilder) { }
-    
-    userPanel = this.fb.group({
-      username: new FormControl(),
-      password: new FormControl()
-    })
+
+  noMoreThan3Appereance(control: AbstractControl): ValidationErrors {
+
+    return { "noMoreThan3Appereance": true }
+  }
+
+
+  userPanel = this.fb.group({
+    username: new FormControl('',
+      [
+        Validators.minLength(5),
+        Validators.required,
+        this.noMoreThan3Appereance
+
+      ]),
+    password: new FormControl('',
+      [
+        Validators.minLength(8),
+        Validators.required,
+      ]
+    )
+  })
 
   // get username(): FormControl{
   //   return this.userPanel.controls['username'] as FormControl
@@ -26,8 +43,9 @@ export class LoginPageComponent implements OnInit {
   //formcontrol formGroup formArray 
   ngOnInit(): void {
     // console.log(this.userList)
-    
-  
+    console.log(this.username)
+
+
   }
 
   onLogin() {
@@ -37,6 +55,17 @@ export class LoginPageComponent implements OnInit {
     this.router.navigate(['newsfeed'])
 
   }
+
+
+  get username(): FormControl {
+    return this.userPanel.controls["username"] as FormControl
+  }
+  get password(): FormControl {
+    return this.userPanel.controls["password"] as FormControl
+  }
+
+
+
 
 
 
